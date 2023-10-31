04a8d23f709327380ee8afd639fc0a9a98c348d3
Shefeena E S <shefeena.e.s@gmail.com>
2023-10-31 18:09:29 +0530

    vercel setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,40 @@
 import TableTasks from "@/components/TableTasks";
 import { db } from "@/lib/db";
-async function getTasks()
-{
-  const response = await db.task.findMany(
-    {
-      select:{
-        id: true,
-        task: true,
-        status: true
-      },
-      orderBy:{
-        createdAt: 'desc'
-      }
+
+// Define an interface for the task
+interface Task {
+  id: number;
+  task: string;
+  status: string;
+}
+
+async function getTasks() {
+  const response = await db.task.findMany({
+    select: {
+      id: true,
+      task: true,
+      status: true
+    },
+    orderBy: {
+      createdAt: 'desc'
     }
-  );
-   // Map the response to the expected structure
-   const tasks = response.map(task => ({
-    id: task.id,
+  });
+
+  // Map the response to the expected structure and convert id to a string
+  const tasks = response.map((task) => ({
+    id: task.id.toString(), // Convert id to a string
     task: task.task,
     status: task.status
   }));
 
   return tasks;
 }
+
 export default async function Home() {
   const tasks = await getTasks();
   return (
-  <main>
-   
-      <TableTasks  tasks={tasks} />
-  
-  </main>
-  )
-};
-
+    <main>
+      <TableTasks tasks={tasks} />
+    </main>
+  );
+}
